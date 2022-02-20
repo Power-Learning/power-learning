5c036f3185fc27b60cf6e33a440d11b256098979
pablo <pdpalma@hotmail.com>
2022-02-20 12:55:55 -0300

    CI initi

---
+++ Assets/LearningTDD/CITests/ShouldSum.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace LearningTDD.CITests
+{
+    public class ShouldSum
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ShouldSumNumbers()
+        {
+            Assert.AreEqual(3,3);
+        }
+    }
+}
